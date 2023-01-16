3eca2f102e0d218912e35c2cc50cc7dfc073de26
Wokhollogram <82580173+Wokhollogram@users.noreply.github.com>
2023-01-16 10:12:18 +0500

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,20 @@
+// Даны инпуты. Сделайте так, чтобы все инпуты по потери фокуса проверяли 
+//свое содержимое на правильное количество символов. Сколько символов 
+//должно быть в инпуте, указывается в атрибуте data-length. Если вбито 
+//правильное количество, то граница инпута становится зеленой, если 
+//неправильное - красной.
+
+var elems = document.getElementsByTagName('input');
+for (var i = 0; i < elems.length; i++) {
+	elems[i].addEventListener('focus', func);
+}
+
+function func() {
+	var correctLength = this.dataset.length; 
+	var inputDataLength = this.value.length; 
+	if(correctLength == inputDataLength){
+		this.style.borderColor = 'green';
+	} else {
+		this.style.borderColor = 'red';
+	}
+}
